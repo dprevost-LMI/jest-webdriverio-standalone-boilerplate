1f92c6384d74214652105d84792f05668b8fd779
David Prevost <david.prevost@goto.com>
2025-07-05 12:56:38 -0400

    Simplify wdio matcher registrations

--- jest.setup.after-env.ts
+++ jest.setup.after-env.ts
@@ -1,17 +1,12 @@
 import { jest, beforeAll, afterAll, expect } from "@jest/globals";
 import { remote } from "webdriverio";
 import { config } from "./wdio.conf.js";
+import { matchers } from "expect-webdriverio";
 
 jest.setTimeout(30000);
 
 // Import and extend Jest's expect with WebdriverIO matchers
 beforeAll(async () => {
-  // Dynamically import expect-webdriverio to extend Jest's expect
-  const expectWebdriverIO = await import("expect-webdriverio");
-  
-  // Get the matchers from expect-webdriverio
-  const matchers = expectWebdriverIO.matchers;
-  
   // Convert the Map to a plain object and extend Jest's expect
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const matchersObject: Record<string, any> = {};
