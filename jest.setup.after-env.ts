c2c04cacfda35384a9196ac590713ff93b5ef26c
David Prevost <david.prevost@goto.com>
2025-07-06 09:15:14 -0400

    Fix matchers bad typings

--- jest.setup.after-env.ts
+++ jest.setup.after-env.ts
@@ -5,16 +5,8 @@ import { matchers } from "expect-webdriverio";
 
 jest.setTimeout(30000);
 
-// Import and extend Jest's expect with WebdriverIO matchers
-beforeAll(async () => {
-  // Convert the Map to a plain object and extend Jest's expect
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const matchersObject: Record<string, any> = {};
-  matchers.forEach((matcher, name) => {
-    matchersObject[name] = matcher;
-  });
-  
-  expect.extend(matchersObject);
+beforeAll(async () => { 
+  expect.extend(matchers);
   
   globalThis.chrome = await remote(config);
 });
